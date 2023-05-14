4c1b1125cb6a2e64a4bb2cb884d44dc8ab51da08
jadso3145 <2345jared@gmail.com>
2023-05-14 13:26:51 -0500

    feat: construyendo la pagina principal

--- app/components/Noticias.tsx
+++ app/components/Noticias.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+"use client";
+
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+
 
 const Noticias = () => {
-  return (
-    <div>Noticias</div>
-  )
-}
+   return (
+      <div className="w-full py-0 px-0 h-[300px] flex justify-center align-middle bg-white text-black">
+         <Swiper className="flex justify-center align-middle">
+            <SwiperSlide>Slide 1</SwiperSlide>
+            <SwiperSlide>Slide 2</SwiperSlide>
+            <SwiperSlide>Slide 3</SwiperSlide>
+            <SwiperSlide>Slide 4</SwiperSlide>
+            <SwiperSlide>Slide 5</SwiperSlide>
+            <SwiperSlide>Slide 6</SwiperSlide>
+            <SwiperSlide>Slide 7</SwiperSlide>
+            <SwiperSlide>Slide 8</SwiperSlide>
+            <SwiperSlide>Slide 9</SwiperSlide>
+         </Swiper>
+         <div className="h-[200px]"></div>
+     
+      </div>
+   );
+};
 
-export default Noticias
+export default Noticias;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Noticias from "@/app/components/Noticias";
+import CardMovies from "./components/CardMovies";
 
 export default function Home() {
    return (
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <main className="flex min-h-screen flex-col items-center justify-between">
          <Noticias />
+         <CardMovies />
       </main>
    );
 }
